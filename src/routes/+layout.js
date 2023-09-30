a808645a7f78137d5f22c9bdfb185568108329ce
Connor Lund <lundcm@users.noreply.github.com>
2023-09-30 13:50:02 -0500

    Login page -- no styling

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,25 @@
-import '../app.css';
-import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+import { dev } from '$app/environment';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import '../app.css';
 
+// Vercel Analytics config
 inject({ mode: dev ? 'development' : 'production' });
+
+export const load = async ({ fetch, data, depends }) => {
+	depends('supabase:auth');
+
+	const supabase = createSupabaseLoadClient({
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		event: { fetch },
+		serverSession: data.session
+	});
+
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+
+	return { supabase, session };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,34 @@
-<h1 class="text-3xl font-bold underline">Welcome to Isembl</h1>
+<script>
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+	export let data;
+</script>
+
+<!-- <h1 class="text-3xl font-bold underline">Welcome to Isembl</h1>
 <p>
-  Visit <a href="https://kit.svelte.dev">How to Start</a> to create a group or team
-  for communication and collaboration.
-</p>
+	Visit <a href="https://kit.svelte.dev">How to Start</a> to create a group or team for communication
+	and collaboration.
+</p> -->
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/auth/callback`}
+			showLinks={false}
+			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+		/>
+	</div>
+</div>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
